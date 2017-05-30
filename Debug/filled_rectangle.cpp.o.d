05f38e238c722fb5fc674d057019c82e42f156e7
BIGStrawberry <wouter.dijkstra.wd@gmail.com>
2017-05-30 14:23:37 +0200

    restructured the folders

--- Debug/filled_rectangle.cpp.o.d
+++
@@ -1,8 +0,0 @@
-Debug/filled_rectangle.cpp.o: filled_rectangle.cpp filled_rectangle.hpp \
- window.hpp line.hpp
-
-filled_rectangle.hpp:
-
-window.hpp:
-
-line.hpp:
--- Release/filled_rectangle.cpp.o.d
+++
@@ -1,8 +0,0 @@
-Release/filled_rectangle.cpp.o: filled_rectangle.cpp filled_rectangle.hpp \
- window.hpp line.hpp
-
-filled_rectangle.hpp:
-
-window.hpp:
-
-line.hpp:
